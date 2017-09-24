5567aa0cb2c6cc3a70934f139effbd57b0943f4d
EDDYCJY <313687982@qq.com>
2017-09-24 21:01:39 +0800

    edit space

--- src/Env.example.php
+++ src/Env.example.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'AccessKeyId' 		=> '',
-	'AccessKeySecret'	=> '',
+	'AccessKeyId'       => '',
+	'AccessKeySecret'   => '',
 	'Format'            => 'json',
 	'Version'           => '2016-07-14',
 	'SignatureMethod'   => 'HMAC-SHA1',
-	'SignatureVersion'	=> '1.0'
+	'SignatureVersion'  => '1.0'
 ];
