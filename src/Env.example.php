c74c9e5821a0623ea8d6fbd27298d92fb3dea24c
eddycjy <313687982@qq.com>
2017-09-24 19:14:38 +0800

    add Env.php example

---
+++ src/Env.example.php
@@ -0,0 +1,9 @@
+<?php
+return [
+	'AccessKeyId' 		=> '',
+	'AccessKeySecret'	=> '',
+	'Format'			=> 'json',
+	'Version'			=> '2016-07-14',
+	'SignatureMethod'	=> 'HMAC-SHA1',
+	'SignatureVersion'	=> '1.0'
+];
